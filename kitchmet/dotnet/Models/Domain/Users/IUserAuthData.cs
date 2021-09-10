3c6301662dcf300d1e6ae47c635cb791c91a8857
Leouel <leouel.guanzon@gmail.com>
2021-09-10 10:33:25 -0700

    uploaded more code samples

---
+++ kitchmet/dotnet/Models/Domain/Users/IUserAuthData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Sabio.Models
+{
+    public interface IUserAuthData
+    {
+        int Id { get; }
+        string Email { get; }
+        IEnumerable<string> Roles { get; }
+        object TenantId { get; }
+        string FirstName { get; }
+        string LastName { get; }
+    }
+}
